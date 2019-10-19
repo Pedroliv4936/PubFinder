7460de2de1899f5fec848aa896871e844e516264
Pedroliv4936 <pedrocaldasdeoliveira@gmail.com>
2019-10-19 14:43:26 +0100

    Erro na abertura do Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
 		jdbc= new JDBC();
 		primaryStage.setTitle("Login");
 		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("keyIcon.png")));
-		Pane root = FXMLLoader.load(getClass().getResource("views/MainScreen.fxml"));
+		Pane root = FXMLLoader.load(getClass().getResource("/PubFinder/src/application/views/MainScreen.fxml"));
 
 		Scene scene = new Scene(root);
 		
