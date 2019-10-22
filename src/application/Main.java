71fcaf751c0c30a28538a674bcf165d1806e6589
Franco Zalamena <francozalamena@gmail.com>
2019-10-22 19:43:11 +0100

    criando as funçoes e objetos necessarios no mainScreen

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,20 +12,34 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	
 	private static Stage currentStage;
+	private static Scene currentScene;
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		primaryStage.setTitle("Login");
 		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("keyIcon.png")));
-		Pane root = FXMLLoader.load(getClass().getResource("views/MainScreen.fxml"));
+		Pane root = FXMLLoader.load(getClass().getResource("views/LoginScreen.fxml"));
 
 		Scene scene = new Scene(root);
+		
+		currentScene = scene;
 		
 		currentStage = primaryStage;
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
+	}
+	
+	public static void changeScene(String fxmlLocation){
+		Pane root=null;
+		try {
+			root = FXMLLoader.load(Main.class.getResource(fxmlLocation));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		currentScene.setRoot(root);
+		currentStage.setScene(currentScene);
 	}
 
 	public static void createNewWindow(String fxmlLocation, String icon) {
@@ -34,13 +48,14 @@ public class Main extends Application {
 		window.setTitle("Pub Finder");
 		Pane root=null;
 		try {
-			root = FXMLLoader.load(Main.class.getResource("views/MainScreen.fxml"));
+			root = FXMLLoader.load(Main.class.getResource(fxmlLocation));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Scene scene = new Scene(root);
 		currentStage= window;
+		currentScene = scene;
 		window.setScene(scene);
 		window.show();
 	}
