44f40966dda344d244a38127c5e05d895d271c49
Franco Zalamena <francozalamena@gmail.com>
2019-10-22 21:59:59 +0100

    Adicionando models ao projeto. Drink e Entity Prontos conforme diagrama de classes

---
+++ src/application/models/Entity.java
@@ -0,0 +1,29 @@
+package application.models;
+
+public abstract class Entity {
+	protected int id;
+	protected String name;
+	protected double rating;
+	
+	public Entity(int id, String name, double rating) {
+		this.id=id;
+		this.name=name;
+		this.rating= rating;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+	
+	}
+	
+	public void rate(double rating) {
+		
+	}
+}
---
+++ src/application/models/Pub.java
@@ -0,0 +1,10 @@
+package application.models;
+
+public class Pub extends Entity{
+
+	public Pub(int id, String name, double rating) {
+		super(id, name, rating);
+		// TODO Auto-generated constructor stub
+	}
+
+}
