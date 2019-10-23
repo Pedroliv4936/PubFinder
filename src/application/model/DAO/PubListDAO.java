d7dea41cf275493beb7fb1f5b3743cf2ff9b22f9
Pedroliv4936 <pedrocaldasdeoliveira@gmail.com>
2019-10-23 14:40:53 +0100

    TableView a funcionar no menu bebidas

--- src/application/model/DAO/PubListDAO.java
+++ src/application/model/DAO/PubListDAO.java
@@ -10,8 +10,14 @@ public class PubListDAO {
 private PubListDAO() {}
 private static ObservableList<Pub> pubList= FXCollections.observableArrayList();
 
+public static ObservableList<Pub> getPubList (){
+	return pubList;
+}
+
 static {
 	ObservableList<Drink> pedroDrinks = FXCollections.observableArrayList();
+	pedroDrinks.add(new Drink(0,"Vodka",4.5,2.5,null));
 	pubList.add(new Pub(1,"Bar do Pedro", 4.5, "Lar das laranjas", pedroDrinks, null));
+	
 }
 }
--- src/application/views/MenuBebidasController.java
+++ src/application/views/MenuBebidasController.java
@@ -1,18 +1,36 @@
 package application.views;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
-import com.lynden.gmapsfx.GoogleMapView;
-import application.Main;
+
+import application.model.DAO.PubListDAO;
+import application.models.Drink;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 
 public class MenuBebidasController {
 
 	@FXML
 	JFXButton bebidasButton,logoButton,userButton;
+	@FXML
+	TableView<Drink> publistTV;
+	@FXML
+	TableColumn<Drink,Image> imageColumn;
+	@FXML
+	TableColumn<Drink,String> drinkColumn;
+	@FXML
+	TableColumn<Drink,String> BarColumn;
+	@FXML
+	TableColumn<Drink,Float> priceColumn;	
 	
 	@FXML
 	private void initialize() {
+		imageColumn.setCellValueFactory(new PropertyValueFactory<Drink, Image>("Image"));
+		drinkColumn.setCellValueFactory(new PropertyValueFactory<Drink, String>("Name"));
+		priceColumn.setCellValueFactory(new PropertyValueFactory<Drink, Float>("Price"));
+		publistTV.setItems(PubListDAO.getPubList().get(0).getDrinks());
 	}
+	
 }
