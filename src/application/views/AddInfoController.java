484702c86df20529d889ea948b49b5f84078d20c
Pedroliv4936 <pedrocaldasdeoliveira@gmail.com>
2019-10-25 11:05:12 +0100

    changes before merge

--- src/application/views/AddInfoController.java
+++ src/application/views/AddInfoController.java
@@ -2,14 +2,31 @@ package application.views;
 
 
 
-import application.ScreenManager;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+
+import application.models.Pub;
 import javafx.fxml.FXML;
 
 
 public class AddInfoController {
+	@FXML
+	JFXTextField tipo,preço;
+	JFXComboBox<Pub> barField;
+	JFXComboBox<String> tipoField;
+	JFXTextArea observacoes;
+	JFXButton enviar;
 	
+	String nomeBar,tipoBebida,preçoEntrada,observações;
 	@FXML
 	private void initialize() {
-
+	
+	}
+	private void NomeBar() {
+	}
+	private void tipoField() {
+		tipoField.getItems().addAll("Discoteca","Pub","Salão de Jogos");
 	}
 }
